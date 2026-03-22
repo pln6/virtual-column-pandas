259e8f6dae9f724e52cb42b91a9d639088a6cf60
Полина Ионина <polinaionina@MacBook-Air-Polina.local>
2026-03-22 18:30:53 +0100

    Implement add_virtual_column logic with sequential ops and * priority

--- src/add_virtual_column.py
+++ src/add_virtual_column.py
@@ -7,13 +7,29 @@ def add_virtual_column(df: pd.DataFrame, role: str, new_column: str) -> pd.DataF
     operations = [role_list[i] for i in range(1, len(role_list), 2)]
     df_result = df.copy()
 
-    for i in range(len(operations)):
-        if i == 0 :
-            df_result[new_column] = op[operations[i]](df_result[columns[i]], df_result[columns[i+1]])
-        else :
-            df_result[new_column] = op[operations[i]](df_result[new_column], df_result[columns[i+1]])
+    columns_final = columns.copy()
+    operations_final = []
 
-    return df_result
+    for i in range(len(operations)):
+        if operations[i] == "*" :
+            df_result[f'tmp{i}'] = op[operations[i]](df_result[columns[i]], df_result[columns[i+1]])
+            idx = columns_final.index(columns[i])
+            cols_to_remove = [columns[i], columns[i+1]]     
+            for col in cols_to_remove:
+                if col in columns_final:
+                    columns_final.remove(col) 
+            columns_final.insert(idx, f'tmp{i}')
+        else :
+            operations_final.append(operations[i])
+
+    result = df_result[columns_final[0]]
+
+    for i, operation in enumerate(operations_final):
+        result = op[operation](result, df_result[columns_final[i+1]])
+    
+    df_result[new_column] = result
+    
+    return df_result.loc[:, df.columns.values.tolist() + [new_column]]
 
 
 
