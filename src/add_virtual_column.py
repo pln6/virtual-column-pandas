b1f89656f5c877380448710aa1828e48233588fc
Полина Ионина <polinaionina@MacBook-Air-Polina.local>
2026-03-22 13:47:47 +0100

    support multilpe sequentioal opertions in add_virtual_column

--- src/add_virtual_column.py
+++ src/add_virtual_column.py
@@ -6,7 +6,14 @@ def add_virtual_column(df: pd.DataFrame, role: str, new_column: str) -> pd.DataF
     columns = [role_list[i] for i in range(0, len(role_list), 2)]
     operations = [role_list[i] for i in range(1, len(role_list), 2)]
     df_result = df.copy()
-    for i in range(len(columns)-1):
-        for operation in operations:
-            df_result[new_column] = op[operation](df[columns[i]], df[columns[i+1]])
+
+    for i in range(len(operations)):
+        if i == 0 :
+            df_result[new_column] = op[operations[i]](df_result[columns[i]], df_result[columns[i+1]])
+        else :
+            df_result[new_column] = op[operations[i]](df_result[new_column], df_result[columns[i+1]])
+
     return df_result
+
+
+
